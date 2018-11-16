7d7036136e57a5c550119458419f61e931bbe7c3
Matthew Farnham <matthewfarnham@cis.vvc.edu>
2018-11-15 19:23:27 -0800

    update

---
+++ deck.cpp
@@ -0,0 +1,17 @@
+/*
+File Name: main.cpp
+Description: It's a deck of cards babyyyy!
+Authors: Ankur Solanky, Luke Schron, Roger Aragon, Matthew Farnham
+Date Created: Thu Nov 15 19:12:57 PST 2018
+*/
+
+#include<iostream>
+#include<ctime>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Deck
+{
+};
